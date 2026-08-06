339be33212d40b7bb8918dc078a730b1b4b44113
murilinds <murilodisep@gmail.com>
2026-08-06 10:15:06 -0300

    Conectando Repositório

---
+++ main.py
@@ -0,0 +1,9 @@
+'''Sistema de Gerenciamento de Biblioteca
+Cadastrar livros (título, autor, ano de publicação, código/ISBN, status: disponível ou emprestado)
+Registrar empréstimo de um livro (muda o status para "emprestado")
+Registrar devolução de um livro (muda o status de volta para "disponível")
+Listar todos os livros cadastrados, com seus status
+Buscar um livro por título ou autor
+Ordenar a listagem de livros (por título, autor ou ano)
+Todos cadastram o mesmo tipo de sistema — a diferença vai estar na qualidade da implementação, na organização do código e em como cada um resolveu os detalhes.
+'''
